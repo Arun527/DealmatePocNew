73f69bee04fc70f73e7de645dc748bb0fc57f964
Karthik1042000 <116278307+Karthik1042000@users.noreply.github.com>
2024-08-20 12:28:29 +0530

    Delete ui directory

--- ui/src/common/constants.ts
+++
@@ -1,4 +0,0 @@
-export const regexPatterns = {
-  alphNum: /^[a-zA-Z0-9]*$/, //allowed alphabets and numbers
-  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, //email validation
-};
--- ui/src/common/env-variables.ts
+++
@@ -1,4 +0,0 @@
-export const environmentalVariable = {
-  // BASE_URL: "https://localhost:7096",
-  BASE_URL: "https://testhost-cxdjewcbddapdbak.eastus-01.azurewebsites.net",
-};
