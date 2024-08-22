207fdb885be302b99ec936673e18d485a9a3670e
Arunachalam S <arunachalam.subramani@amtote.com>
2024-08-22 13:51:11 +0530

    feat: add migration

--- DealmateApi/Migrations/20240822082046_add-coloumnin-vechile.Designer.cs
+++ DealmateApi/Migrations/20240822082046_add-coloumnin-vechile.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DealmateApi.Infrastructure.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DealmateApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240822082046_add-coloumnin-vechile")]
+    partial class addcoloumninvechile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DealmateApi/Migrations/20240822082046_add-coloumnin-vechile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DealmateApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcoloumninvechile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Active",
+                table: "Vehicle",
+                type: "interval",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Vehicle");
+        }
+    }
+}
