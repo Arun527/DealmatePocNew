586de53f5062aa78f29d5bd62085a558ff6a84b5
Arunachalam S <arunachalam.subramani@amtote.com>
2024-08-29 13:13:54 +0530

    change cs

--- DealmateApi/Program.cs
+++ DealmateApi/Program.cs
@@ -1,15 +1,20 @@
 using DealmateApi.Infrastructure;
 using DealmateApi.Infrastructure.DB;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration;
 // Determine the environment
 var environment = builder.Environment.EnvironmentName;
 // Load the base configuration from appsettings.json
-var configuration = builder.Configuration;
+var configFileName = $"appsettings.{environment}.json";
 
-var envConfigSection = configuration.GetSection($"Environments:{environment}");
-builder.Configuration.AddConfiguration(envConfigSection);
+configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(configFileName, optional: true, reloadOnChange: true)
+    .Build();
 // Add services to the container
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
