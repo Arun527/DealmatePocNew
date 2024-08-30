e2d177cec32b656abe5691aff623c686bd52d976
Karthik P <prathapm@amtote.com>
2024-08-30 14:19:22 +0530

    cors error

--- DealmateApi/Program.cs
+++ DealmateApi/Program.cs
@@ -30,7 +30,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigins", builder =>
     {
         builder
-        .AllowAnyOrigin()
+        .WithOrigins("http://localhost:3000",
+        "https://dealmatedevelopment-ui-gbfpfxeyh4eqhxgr.eastasia-01.azurewebsites.net") // Replace with your client application's URL
         .AllowAnyHeader()
         .AllowAnyMethod()
         .AllowCredentials(); // Allow credentials
